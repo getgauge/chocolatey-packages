bf08d699e7fb2dc81e844a203bdb9001c96afe84
nehashri <nehashrl@thoughtworks.com>
2018-10-22 18:31:05 +0530

    adding checks when command fails

--- gauge/package.ps1
+++ gauge/package.ps1
@@ -1,3 +1,10 @@
 $version = $args[0]
 (Get-Content tools\chocolateyInstall.ps1.template).replace('{{version}}', $version) | Set-Content tools\chocolateyInstall.ps1;
+if ($LastExitCode -ne 0) {
+     throw "Failed to replace version in template"
+}
+
 choco pack --version=$version
+if ($LastExitCode -ne 0) {
+     throw "Choco pack failed"
+}
