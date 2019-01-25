cdf6940b5f410bc610ba0e39da3021dc399f148d
Dharmendra Singh <dharmenn@thoughtworks.com>
2019-01-25 17:02:03 +0530

    Fixed package.ps1

--- gauge/package.ps1
+++ gauge/package.ps1
@@ -1,10 +1,8 @@
-$version = $args[0]
-(Get-Content tools\chocolateyInstall.ps1.template).replace('{{version}}', $version) | Set-Content tools\chocolateyInstall.ps1;
-if ($LastExitCode -ne 0) {
-     throw "Failed to replace version in template"
-}
-
-choco pack --version=$version
-if ($LastExitCode -ne 0) {
-     throw "Choco pack failed"
+gc ..\version | % {
+    (Get-Content tools\chocolateyInstall.ps1.template).replace('{{version}}', $_) | Set-Content tools\chocolateyInstall.ps1;
+  
+    choco pack --version=$_
+    if ($LastExitCode -ne 0) {
+        throw "Choco pack failed"
+    }
 }
