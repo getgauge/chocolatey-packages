406b9d7614163c6f21de723737bd3e028783df34
Chad Wilson <chadw@thoughtworks.com>
2024-02-10 15:27:15 +0800

    Correct Chocolatey package to include checksums

--- gauge/package.ps1
+++ gauge/package.ps1
@@ -1,6 +1,21 @@
 gc ..\version | % {
-    (Get-Content tools\chocolateyInstall.ps1.template).replace('{{version}}', $_) | Set-Content tools\chocolateyInstall.ps1;
-  
+    Write-Host "Templating Chocolatey package for Gauge version $_"
+
+    $packageDir = "..\..\deploy"
+    $package = "gauge-$_-windows.x86.zip"
+    $package64 = "gauge-$_-windows.x86_64.zip"
+
+    $checksum = (Get-FileHash -Path (Join-Path $packageDir $package) -Algorithm SHA256).Hash.ToLower()
+    $checksum64 = (Get-FileHash -Path (Join-Path $packageDir $package64) -Algorithm SHA256).Hash.ToLower()
+
+    (Get-Content tools\chocolateyInstall.ps1.template) `
+        -replace '{{url}}', "https://github.com/getgauge/gauge/releases/download/v$_/$package" `
+        -replace '{{checksum}}', "$checksum" `
+        -replace '{{url64}}', "https://github.com/getgauge/gauge/releases/download/v$_/$package64" `
+        -replace '{{checksum64}}', "$checksum64" `
+            | Set-Content tools\chocolateyInstall.ps1;
+
+    Write-Host "Templating done. Packaging..."
     choco pack --version=$_
     if ($LastExitCode -ne 0) {
         throw "Choco pack failed"
